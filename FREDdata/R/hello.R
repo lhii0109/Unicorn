41f88c28b7c93be0aba8ec0ba31b19a0947ceebf
Vincent Paris <vincentparis@anarchistoparis.student.iastate.edu>
2017-03-30 17:58:25 -0400

    Some light corrections on functions

--- FREDdata/R/hello.R
+++ FREDdata/R/hello.R
@@ -1,26 +1,15 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
+
+
+
 
 
 FRED.CPI <- read.csv("./Data/CPIAUCSL.csv")
 
 
-inflation.through.time <- function(x,y){
+inflation.through.time <- function(beginning.date,ending.date){
   FRED.CPI$DATE <- ymd(FRED.CPI$DATE)
- bottom.cut <- FRED.CPI %>% filter(DATE >paste(x))
- final.cut <- bottom.cut %>% filter(DATE < paste(y))
+ bottom.cut <- FRED.CPI %>% filter(DATE >paste(beginning.date))
+ final.cut <- bottom.cut %>% filter(DATE < paste(ending.date))
 ggplot(final.cut, aes(DATE, CPIAUCSL)) + geom_line()
 }
 
