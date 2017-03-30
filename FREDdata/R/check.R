801384c35967480cd5df287fb49e9b48b14da2b8
jeffsong9 <jeffsong9@gmail.com>
2017-03-30 16:39:35 -0500

    push this

--- FREDdata/R/check.R
+++ FREDdata/R/check.R
@@ -1,1 +1,14 @@
-# check if I could push
+pkg=c("lubridate", "tidyverse")
+sapply(pkg, require, character=T)
+
+FRED=read.csv("../Data/CPIAUCSL.csv", stringsAsFactors = F)
+
+closest_date=function(Date){
+  x=ymd(FRED$DATE)
+  FRED[which(abs(x-ymd(Date))==min(abs(x-ymd(Date)))),] %>% 
+    return()
+  
+}
+
+# closest_date("1988-09-09")
+
